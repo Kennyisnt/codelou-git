a55d3d1f766faff9c8122eb3cda5159c46c6d1c8
Kenny <Kenneth.isnt@hotmail.com>
2019-02-06 16:13:30 -0500

    Turned Off Prompts for JS.

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,13 +1,12 @@
 
 var Pokemon = ['Voltorb', 'Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', "Farfetch'd", 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler']
-var Month = prompt("Please Enter The Month You were Born 01-January, 12-December");
-var Day = prompt("Please enter the day that you were born 01, 02, 03... 31");
-var Year = prompt("Please enter the 2 diget year that you were born. (1999 = '99', 2000 = '00')");
+
+// Turning Off Prompts
+// var Month = prompt("Please Enter The Month You were Born 01-January, 12-December");
+// var Day = prompt("Please enter the day that you were born 01, 02, 03... 31");
+// var Year = prompt("Please enter the 2 diget year that you were born. (1999 = '99', 2000 = '00')");
+
 Month = parseInt(Month);
 Day = parseInt(Day);
 Year = parseInt(Year);
 document.write("<h1>Your Team Is .. " + Pokemon[Month] + " " + Pokemon[Day] + " " + Pokemon[Year] + "</h1>");
-
-// for (i = 0; i < Pokemon.length; i++) { 
-//   console.log(Pokemon[i]);
-// }
