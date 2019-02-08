b39600bb74def09565ba813ecb8100e902aee6d3
Kenny <Kenneth.isnt@hotmail.com>
2019-02-08 18:43:34 -0500

    Final Changes 02/08/19

--- SchoolProjects/js.js
+++ SchoolProjects/js.js
@@ -8,6 +8,8 @@ const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 
+const removeItemButton = document.querySelector('button.removeItemButton');
+
 //Toggles The List On and Off
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
@@ -22,12 +24,23 @@ toggleList.addEventListener('click', () => {
 //Writes to Paragraph the contents of the TEXT Field for List Description
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = "";
 });
 
-//Add HTML Item Button!
+//Add HTML Items to a list (button)
 
 addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
-  
+  ul.appendChild(li);
+  addItemInput.value = '';
+});
+
+//Remove lastitem from a list (Button)
+
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
 });
