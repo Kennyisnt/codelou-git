517a0089b203b614819e4e2be266d826af15704e
Kenny <Kenneth.isnt@hotmail.com>
2019-02-12 13:35:25 -0500

    Created a Folder/ moved files and saved final DOM project files.

--- SchoolProjects/DOM Stuff/js.js
+++ SchoolProjects/DOM Stuff/js.js
@@ -9,29 +9,61 @@ const listUL = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 
-
 const listItems = document.getElementsByTagName('li');
+const lis = listUL.children;
+const firstListItem = listUL.firstElementChild;
+const lastListItem = listUL.lastElementChild;
+firstListItem.style.backgroundColor = 'coral';
+lastListItem.style.backgroundColor = 'lime';
 
 
-//hover to uppercase!
+//sets button rules
+function attachListItemButtons(li) {
+  let up = document.createElement('button');
+  up.className="up";
+  up.textContent="up";
+  li.appendChild(up);
+  let down = document.createElement('button');
+  down.className="down";
+  down.textContent="down";
+  li.appendChild(down);
+  let remove = document.createElement('button');
+  remove.className="remove";
+  remove.textContent="remove";
+  li.appendChild(remove);
+}
+
+//adds buttons in HTML
+for (let i = 0; i < lis.length; i += 1) {
+  attachListItemButtons(lis[i]);
+
+}
+
+//Makes Down/Up/Remove buttons work
 listUL.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
-    // event.target.textContent = event.target.textContent.toUpperCase();
-    let li = event.target.parentNode;
-    let ul = li.parentNode;
-    ul.removeChild(li);
+    if (event.target.className=="remove") {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
+    if (event.target.className=="up") {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      let ul = li.parentNode;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+    }}
+    if (event.target.className=="down") {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      let ul = li.parentNode;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+    }}
+
   }
 });
-
-//unhover to unuppercase
-// listDiv.addEventListener('mouseout', (event) => {
-//   if (event.target.tagName == 'LI') {
-//     event.target.textContent = event.target.textContent.toLowerCase();
-//     }
-// });
-
-
-
 
 
 
@@ -57,6 +89,7 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
