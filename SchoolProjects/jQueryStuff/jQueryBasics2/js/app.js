89c3ad99fc4c5f722792e1b9433d94f2d4debb0f
Kenny <Kenneth.isnt@hotmail.com>
2019-02-15 19:03:56 -0500

    Spoiler button works!

--- SchoolProjects/jQueryStuff/jQueryBasics2/js/app.js
+++ SchoolProjects/jQueryStuff/jQueryBasics2/js/app.js
@@ -1,5 +1,9 @@
-
-
-
-
-
+//hide the spoiler text
+$('.spoiler span').hide();
+//when the button is pressed
+$('.spoiler button').click(function() {
+  //show the spoiler text
+  $('.spoiler span').show();
+  //hide the spoiler button
+  $('.spoiler button').hide();
+});
