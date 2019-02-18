5598306b44b92e4a908dffa95e9667e1d22274db
Kenny <Kenneth.isnt@hotmail.com>
2019-02-18 18:55:32 -0500

    updated app.js in jquerybasics2 - better event handler

--- SchoolProjects/jQueryStuff/jQueryBasics2/js/app.js
+++ SchoolProjects/jQueryStuff/jQueryBasics2/js/app.js
@@ -1,9 +1,19 @@
+
+
+$('.spoiler').on('click', 'button', function(event) {
+  console.log(event.target);
+  //show the spoiler text
+  //$('.spoiler span').show();
+  //hide the spoiler button
+  $(event.target).hide();
+});
+
+
+//Create the 'Reveal Spoiler Button
+const $button = $('<button>Reveal Spoiler</button>');
+//Append to Webpage
+$('.spoiler').append($button);
+
 //hide the spoiler text
 $('.spoiler span').hide();
 //when the button is pressed
-$('.spoiler button').click(function() {
-  //show the spoiler text
-  $('.spoiler span').show();
-  //hide the spoiler button
-  $('.spoiler button').hide();
-});
