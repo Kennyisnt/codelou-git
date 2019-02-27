3e03d038a81045a01b50afb5582b1d8f6caa57b1
Kenny <Kenneth.isnt@hotmail.com>
2019-02-26 19:13:21 -0500

    Commit EOD 02/26/19

--- SchoolProjects/jQueryStuff/jQueryBasics3/js/app.js
+++ SchoolProjects/jQueryStuff/jQueryBasics3/js/app.js
@@ -2,4 +2,23 @@ const $odd = $('a:odd');
 const $secureLinks = $('a[href ^= "https://"]');
 const $unSecureLinks = $('a[href ^= "http://"]');
 const $pdfs = $('a[href $=".pdf"]');
-$pdfs.hide();
+// $pdfs.hide();
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
+
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+$('#links').append($pdfCheckbox);
+
+$pdfs.on('click', function(event) {
+  //check if checkbox has been checked
+  if ($(':checked').length === 0) {
+    //if zero checkboxes are chekded
+    event.preventDefault();
+    //prevent download of Documentation
+    alert('Please check the box to allow PDF downloads.');
+    //alert the $unSecureLinks
+  };
+});
