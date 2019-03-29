75a7d689c033cbc20a4bc4c6c67fc28b183cac71
Kenny <Kenneth.isnt@hotmail.com>
2019-03-29 00:53:23 -0400

    Removed Misc File Overhead / Prepping for Project Turn in.

--- archive/WebPages/DerpsNDergens/README.md
+++
@@ -1,2 +0,0 @@
-# codelou-git
-This is a projet for Code Louisville. This will be a personal portfolio page that will expand as I continue my coding/front end development career. 
--- archive/WebPages/DerpsNDergens/js.js
+++
@@ -1,6 +0,0 @@
-// var temperatures = [100,90,99,80,70,65,30,10];
-//
-// for (var i = 0; i < temperatures.length; i++) {
-// 	document.write(temperatures[i]);
-//   document.write(",");
-// }
